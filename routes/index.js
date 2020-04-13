0271677283f376ef2441bdf8c7303a260b70ac23
Simon Fish <si@mon.fish>
2020-04-13 16:27:47 +0100

    Render navbar from JSON file
    To add items to the navbar, add to the JSON array at
    `views/data/navbar.json`. This may need to expand if we're also loading
    the user dropdown from that JSON file, but there may not be a need to.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
+var navbar = require("../views/data/navbar.json")
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express", path: req.path })
+  res.render("index", { title: "Express", path: req.path, navbar: navbar })
 });
 
 module.exports = router;
