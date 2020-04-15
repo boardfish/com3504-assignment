f393644078be6451879307d3fea8e6ccf06e3972
Simon Fish <si@mon.fish>
2020-04-15 16:48:56 +0100

    Prepare AJAX request

--- public/javascripts/likeButton.js
+++ public/javascripts/likeButton.js
@@ -1,8 +1,22 @@
+// from the slides for week 3
+const sendAjaxQuery = (url, data) => {
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: data,
+    context: this,
+    contentType: 'application/json',
+    error: () => { alert('Failure') },
+    success: () => { alert('Success') },
+  })
+}
+
 const buttons = document.querySelectorAll("span.fivestars");
-const submitLike = (postId, value) => {
-  alert(postId);
-  alert(value);
+
+const submitLike = (storyId, vote) => {
+  sendAjaxQuery(`http://localhost:4000/stories/${storyId}/rate/${vote}`, {})
 };
+
 buttons.forEach((button) =>
   button.addEventListener("click", () => {
     submitLike(button.dataset.postId, button.dataset.value);
