ec662b3cec570f0d9a1442b07317aa2a751ad58c
Simon Fish <si@mon.fish>
2020-04-29 10:21:31 +0100

    Display time at which post was made

--- models/story.js
+++ models/story.js
@@ -6,7 +6,7 @@ var Story = new Schema({
   user: { type: Schema.Types.ObjectID, ref: "User", required: true },
   text: { type: String, max: 200 },
   likes: [{ type: Schema.Types.ObjectID, ref: "Like", required: true }],
-});
+}, { timestamps: true });
 
 const storyModel = mongoose.model("Story", Story);
 
