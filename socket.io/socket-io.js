62f69d27eb51859f2fc5fd4b127eafb95db38206
MatthewS11119 <matthewsaddress2018@gmail.com>
2020-04-29 11:08:15 +0100

    More socket.io

--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,5 +1,15 @@
 exports.init = function (io, app){
-  io.sockets.on('connection', function (credentials, message, parameter) {
-    
+  io.sockets.on('connection', function (socket) {
+    try {
+      socket.on('custom-message', function (message, parameter) {
+        socket.broadcast.emit('custom-message', message, parameter);
+      });
+
+      socket.on('acuityClick', function (id) {
+        socket.broadcast.emit('acuityClick', id);
+      });
+    } catch (err){
+      err.print();
+    }
   })
-}
+};
