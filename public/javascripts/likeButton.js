6d6635ecbbbb4801330acf08988c214051725065
Simon Fish <si@mon.fish>
2020-05-04 16:55:35 +0100

    Use jQuery on() to fix like button interaction
    on() takes a selector and runs for anything using that selector. It's
    even responsive to changes in the DOM, which is what was letting us down
    - posts aren't initially in the DOM even before $(document).ready(), so
    we need to use on() to respond to those changes.

--- public/javascripts/likeButton.js
+++ public/javascripts/likeButton.js
@@ -11,8 +11,6 @@ const sendAjaxQuery = (url, data, errorCallback, successCallback) => {
   });
 };
 
-const buttons = document.querySelectorAll("span.fivestars");
-
 const submitLike = (storyId, vote) => {
   sendAjaxQuery(
     `/stories/${storyId}/rate/${vote}`,
@@ -21,16 +19,23 @@ const submitLike = (storyId, vote) => {
       alert("Error");
     },
     () => {
-      $(`span.fivestars[data-story-id=${storyId}]`).removeClass(Array(5).fill().map((_, i) => `text-${i+1}-star`).join(' '))
-      $(`span.fivestars[data-story-id=${storyId}]:nth-child(-n+${vote})`).addClass(`text-${vote}-star`)
-      $(`span.fivestars[data-story-id=${storyId}]`).removeClass('selected')
-      $(`span.fivestars[data-story-id=${storyId}]:nth-child(${vote})`).addClass('selected')
+      $(`span.fivestars[data-story-id=${storyId}]`).removeClass(
+        Array(5)
+          .fill()
+          .map((_, i) => `text-${i + 1}-star`)
+          .join(" ")
+      );
+      $(
+        `span.fivestars[data-story-id=${storyId}]:nth-child(-n+${vote})`
+      ).addClass(`text-${vote}-star`);
+      $(`span.fivestars[data-story-id=${storyId}]`).removeClass("selected");
+      $(`span.fivestars[data-story-id=${storyId}]:nth-child(${vote})`).addClass(
+        "selected"
+      );
     }
   );
 };
 
-buttons.forEach((button) =>
-  button.addEventListener("click", () => {
-    submitLike(button.dataset.storyId, button.dataset.value);
-  })
-);
+$(document).on("click", "span.fivestars", (e) => {
+  submitLike(e.target.dataset.storyId, e.target.dataset.value);
+});
