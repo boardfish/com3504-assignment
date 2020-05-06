bf08637f75416b8d7a14225a1ad1516ba7d1673e
Simon Fish <si@mon.fish>
2020-05-06 11:03:52 +0100

    Lint, add User.passwordIsCorrect()

--- models/users.js
+++ models/users.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const User = new Schema({
   username: { type: String, required: true, max: 100 },
   password: { type: String, required: true, max: 100 },
   email: { type: String, required: true, max: 200 },
   nickname: { type: String },
-})
+});
 
-const userModel = mongoose.model("User", User)
+User.methods.passwordIsCorrect = (passwordInput) =>
+  this.password === passwordInput;
 
-module.exports = userModel
+const userModel = mongoose.model("User", User);
+
+module.exports = userModel;
