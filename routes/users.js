f460b1268f9a221deba038bf1beeaa8073f89611
Simon Fish <si@mon.fish>
2020-05-07 10:58:58 +0100

    Get sign_in endpoint working

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,17 @@
-var express = require("express")
-var router = express.Router()
-var stories = require("../controllers/story")
-var users = require("../controllers/users")
+var express = require("express");
+var router = express.Router();
+var stories = require("../controllers/story");
+var users = require("../controllers/users");
+var passport = require("passport");
 
 /* GET users listing. */
-router.get("/:userId/stories", stories.getAllUserStories)
-router.post("/sign_in", users.signIn)
+router.get("/:userId/stories", stories.getAllUserStories);
+router.post(
+  "/sign_in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/youfuckedup"
+  })
+);
 
-module.exports = router
+module.exports = router;
