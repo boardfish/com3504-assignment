a75f8aa216fbf6df0188a8107e8bac0dccc9af97
Simon Fish <si@mon.fish>
2020-05-07 09:58:42 +0100

    get password verification method working

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,9 @@
-var Users = require("../models/users")
+var Users = require("../models/users");
 
 exports.insert = function (req, res) {
-  var userData = req.body
+  var userData = req.body;
   if (userData == null) {
-    res.status(403).send("No data sent!")
+    res.status(403).send("No data sent!");
   }
   try {
     var user = new Users({
@@ -11,41 +11,44 @@ exports.insert = function (req, res) {
       password: userData.password,
       email: userData.email,
       nickname: userData.nickname,
-    })
+    });
 
-    console.log("received + " + user)
+    console.log("received + " + user);
 
     user.save(function (err, user) {
-      console.log(user._id)
+      console.log(user._id);
       if (err) {
-        res.status(500).send("Invalid data!")
-        return
+        res.status(500).send("Invalid data!");
+        return;
       }
-      res.setHeader("Content-Type", "application/json")
-      res.send(JSON.stringify(user))
-    })
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(user));
+    });
   } catch (e) {
-    res.status(500).send("error" + e)
+    res.status(500).send("error" + e);
   }
-}
+};
 
 exports.signIn = function (req, res) {
-  Users.findOne({ username: req.body.username }).exec(
-    (err, user) => {
-      if (user == null) {
-        res.setHeader("Content-Type", "application/json")
-        res.status(403).send()
-      } else if (user.passwordIsCorrect(req.body.password)) {
-        console.log(user)
-        console.log(req.body)
-        res.setHeader("Content-Type", "application/json")
-        res.status(200).send(user)
-      } else {
-        console.log(user)
-        console.log(req.body)
-        res.setHeader("Content-Type", "application/json")
-        res.status(403).send()
-      }
+  Users.findOne({ username: req.body.username }).exec((err, user) => {
+    if (user == null) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(403).send();
+    } else {
+      user.passwordIsCorrect(req.body.password, (err, isMatch) => {
+        if (err) {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send();
+          return;
+        }
+        if (!isMatch) {
+          res.setHeader("Content-Type", "application/json");
+          res.status(403).send();
+          return;
+        }
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(user);
+      });
     }
-  )
-}
+  });
+};
--- models/users.js
+++ models/users.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const bcrypt = require("bcrypt");
+
 const User = new Schema({
   username: { type: String, required: true, max: 100 },
   password: { type: String, required: true, max: 100 },
@@ -10,9 +12,35 @@ const User = new Schema({
 });
 
 // Can't use arrow functions here - they prevent binding `this`
-User.methods.passwordIsCorrect = function (passwordInput) {
-  return (this.password === passwordInput);
-}
+User.pre("save", function (next) {
+  // only hash the password if it has been modified (or is new)
+  var user = this;
+  if (!(user.isModified("password") || user.isNew())) {
+    return next()
+  };
+
+  // generate a salt
+  bcrypt.genSalt((err, salt) => {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      console.log(hash, user.password)
+      next();
+    });
+  });
+});
+
+User.methods.passwordIsCorrect = function (passwordInput, callback) {
+  bcrypt.compare(passwordInput, this.password, function(err, isMatch) {
+    if (err) { return callback(err) };
+    return callback(null, isMatch);
+  })
+};
 
 const userModel = mongoose.model("User", User);
 
