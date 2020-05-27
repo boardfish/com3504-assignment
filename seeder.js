6e0e39bcfa5809d3c6a6d3dd6c8c546d227d1d35
boardfish <boardfish@users.noreply.github.com>
2020-05-27 16:05:11 +0000

    Prettified Code!

--- seeder.js
+++ seeder.js
@@ -1,24 +1,24 @@
 // Duplicated from users.js
-const mongoose = require("mongoose");
-var inputData = require("./usersStoriesAndRatings.json");
-var User = require("./models/users");
-var Story = require("./models/story");
-var Likes = require("./models/likes");
+const mongoose = require("mongoose")
+var inputData = require("./usersStoriesAndRatings.json")
+var User = require("./models/users")
+var Story = require("./models/story")
+var Likes = require("./models/likes")
 
 // Takes a story's text and returns the first matching ID
 const findStoryById = (id) =>
-  inputData.stories.find((story) => story.storyId === id);
+  inputData.stories.find((story) => story.storyId === id)
 
 mongoose.connect(
   process.env.MONGO_CONNECTION_STRING ||
     "mongodb://localhost:27017/development",
   { useNewUrlParser: true, useUnifiedTopology: true }
-);
-let db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+)
+let db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", function () {
   // we're connected!
-  console.log("Database connected");
+  console.log("Database connected")
   User.insertMany(
     inputData.users.map(({ userId }) => ({
       username: userId,
@@ -28,10 +28,10 @@ db.once("open", function () {
     })),
     (err) => {
       if (err) {
-        console.log(err);
+        console.log(err)
       }
       User.count({}, function (err, userCount) {
-        console.log(userCount, inputData.users.length);
+        console.log(userCount, inputData.users.length)
         Promise.all(
           inputData.stories.map(async ({ userId, text }) => ({
             user: await User.findOne({ username: userId }),
@@ -40,13 +40,13 @@ db.once("open", function () {
         ).then((stories) => {
           Story.insertMany(stories, (err) => {
             if (err) {
-              console.log(err);
+              console.log(err)
             }
             Story.count({}, function (err, storyCount) {
-              console.log(storyCount, inputData.stories.length);
+              console.log(storyCount, inputData.stories.length)
               Promise.all(
                 inputData.users.map(async ({ userId, ratings }) => {
-                  var user = await User.findOne({ username: userId });
+                  var user = await User.findOne({ username: userId })
                   return Promise.all(
                     ratings.map(async ({ storyId, rating }) => ({
                       user,
@@ -55,13 +55,13 @@ db.once("open", function () {
                       }), // TODO }),
                       vote: rating - 1,
                     }))
-                  );
+                  )
                 })
               )
                 .then((likes) => {
                   Likes.insertMany([].concat.apply([], likes), (err) => {
                     if (err) {
-                      console.log(err);
+                      console.log(err)
                     }
                     Likes.count({}, function (err, likesCount) {
                       console.log(
@@ -70,19 +70,19 @@ db.once("open", function () {
                           [],
                           inputData.users.map(({ ratings }) => ratings)
                         ).length
-                      );
-                      return mongoose.disconnect();
-                    });
-                  });
+                      )
+                      return mongoose.disconnect()
+                    })
+                  })
                 })
                 .catch((err) => {
-                  console.log(err);
-                  return mongoose.disconnect();
-                });
-            });
-          });
-        });
-      });
+                  console.log(err)
+                  return mongoose.disconnect()
+                })
+            })
+          })
+        })
+      })
     }
-  );
-});
+  )
+})
