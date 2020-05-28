28242706d482c4d2780674d569e36668ad42f48c
Simon Fish <si@mon.fish>
2020-05-28 19:06:14 +0100

    Document userSignInForm.js arrow functions

--- public/javascripts/userSignInForm.js
+++ public/javascripts/userSignInForm.js
@@ -1,3 +1,8 @@
+/**
+ * This function sends a POST request to /users/sign_in with the supplied data.
+ * If this is successful, the page is reloaded.
+ * @param {object} data a JS object containing fields username and password.
+ */
 const signIn = (data) => {
   sendAjaxQuery(
     `/users/sign_in`,
@@ -5,7 +10,7 @@ const signIn = (data) => {
     () => {
       alert("We couldn't sign you in. Please try again.");
     },
-    (data) => {
+    () => {
       window.location.reload();
       signInForm.slideUp();
     }
