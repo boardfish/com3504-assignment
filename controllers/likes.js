a2165e14cb551832682da9b16262530ee4160b73
Simon Fish <si@mon.fish>
2020-05-28 19:45:44 +0100

    Document likes controller

--- controllers/likes.js
+++ controllers/likes.js
@@ -2,6 +2,13 @@ var Like = require("../models/likes")
 var Story = require("../models/story")
 var utils = require("./utils")
 
+/**
+ * This controller action creates a new Like object linked to the story with the
+ * ID specified by the storyId parameter on the request. A vote from 1 to 5
+ * inclusive should also be supplied.
+ * @param {Request} req a Request object
+ * @param {Response} res a Response object
+ */
 exports.insert = function (req, res) {
   var like = new Like({
     story: req.params.storyId,
