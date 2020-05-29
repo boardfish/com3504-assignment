d55aa11144b88937d0824dcd2a6961ab8dc1d7be
boardfish <boardfish@users.noreply.github.com>
2020-05-29 11:56:45 +0000

    Prettified Code!

--- controllers/likes.js
+++ controllers/likes.js
@@ -14,26 +14,34 @@ exports.insert = function (req, res) {
   var data = {
     story: req.params.storyId,
     vote: req.params.vote - 1,
-    user: req.user._id
+    user: req.user._id,
   }
   console.log("received: " + data)
-  Like.findOneAndUpdate({ user: req.user._id, story: req.params.storyId }, data, {
-    new: true, upsert: true
-  }, function (err, like) {
-    if (err) {
-      utils.render(req, res, "friendly-error", 200, err, like, {})
-      return
-    }
-    console.log("Like: ", like)
-    Story.findByIdAndUpdate(like.story, { $addToSet: { likes: like } }, { new:true }, function (
-      err,
-      _story
-    ) {
+  Like.findOneAndUpdate(
+    { user: req.user._id, story: req.params.storyId },
+    data,
+    {
+      new: true,
+      upsert: true,
+    },
+    function (err, like) {
       if (err) {
-        console.log(err)
+        utils.render(req, res, "friendly-error", 200, err, like, {})
+        return
       }
-      console.log(_story)
-      utils.render(req, res, "friendly-error", 200, err, data, {})
-    })
-  })
+      console.log("Like: ", like)
+      Story.findByIdAndUpdate(
+        like.story,
+        { $addToSet: { likes: like } },
+        { new: true },
+        function (err, _story) {
+          if (err) {
+            console.log(err)
+          }
+          console.log(_story)
+          utils.render(req, res, "friendly-error", 200, err, data, {})
+        }
+      )
+    }
+  )
 }
